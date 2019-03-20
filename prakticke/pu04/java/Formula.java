b785ab70b80efc7ff9d3479365575b132384a15d
BenkovaVeronika <47613107+BenkovaVeronika@users.noreply.github.com>
2019-03-20 21:02:04 +0100

    Add files via upload

--- prakticke/pu04/java/Formula.java
+++ prakticke/pu04/java/Formula.java
@@ -7,8 +7,11 @@ import java.util.stream.Stream;
 
 public interface Formula {
     public Formula[] subf();
+
     public String toString();
+
     public boolean equals(Formula other);
+
     public Set<String> vars();
 
     public Cnf toCnf();
@@ -37,7 +40,7 @@ class Variable implements Formula {
     public boolean equals(Formula other) {
         if (this == other) return true;
         if (getClass() != other.getClass()) return false;
-        Variable otherVar = (Variable)other;
+        Variable otherVar = (Variable) other;
         return name().equals(otherVar.name());
     }
 
@@ -54,35 +57,41 @@ class Variable implements Formula {
         return "_x" + ++newNum;
     }
 
+    @Override
     public Cnf toCnf() {
-        /* TODO Implement this! */
-        return new Cnf();
+        Cnf cnf = new Cnf();
+        cnf.add(new Clause(new Literal(name)));
+        return cnf;
     }
 }
 
 class CompositeFormula implements Formula {
     Formula[] subs;
     String conn;
+
     CompositeFormula(Formula[] subs, String conn) {
         this.subs = subs;
         this.conn = conn;
     }
-    public Formula[] subf() { return subs; }
+
+    public Formula[] subf() {
+        return subs;
+    }
 
     public String toString() {
         return "("
-            + Arrays.stream(this.subf())
-                .map( f -> f.toString() )
+                + Arrays.stream(this.subf())
+                .map(f -> f.toString())
                 .collect(Collectors.joining(this.conn))
-            + ")"
-        ;
+                + ")"
+                ;
     }
 
     public boolean equals(Formula other) {
         if (this == other) return true;
         if (getClass() != other.getClass()) return false;
         if (subf().length != other.subf().length) return false;
-        for (int i=0; i < subf().length; ++i)
+        for (int i = 0; i < subf().length; ++i)
             if (!subf()[i].equals(other.subf()[i])) return false;
         return true;
     }
@@ -113,11 +122,64 @@ class Negation extends CompositeFormula {
     public String toString() {
         return "-" + originalFormula().toString();
     }
+
+    @Override
+    public Cnf toCnf() {
+        Cnf cnf = new Cnf();
+        String a = Variable.newName();
+        Literal l = Literal.Lit(a);
+        Clause c = new Clause(l);
+        cnf.add(c);
+        Cnf cnf2 = originalFormula().toCnf();
+
+        String b = cnf2.remove(0).remove(0).name();
+        cnf.addAll(cnf2);
+        Literal nota = Literal.Not(a);
+        Literal notb = Literal.Not(b);
+        Literal yesa = Literal.Lit(a);
+        Literal yesb = Literal.Lit(b);
+        Clause first = new Clause(nota,notb);
+        Clause second = new Clause(yesa,yesb);
+        cnf.add(first); cnf.add(second);
+        return cnf;
+    }
 }
 
 class Conjunction extends CompositeFormula {
     public Conjunction(Formula[] formulas) {
         super(formulas, "&");
+    }
+    @Override
+    public Cnf toCnf() {
+        Cnf cnf = new Cnf();
+        String a = Variable.newName();
+        Literal l = Literal.Lit(a);
+        Clause c = new Clause(l);
+        Clause c2 = new Clause(Literal.Lit(a));
+        cnf.add(c);
+        ArrayList<String> vysl = new ArrayList<>();
+
+
+        for (int i = 0; i < subs.length; i++) {
+            Cnf pom = subs[i].toCnf();
+            vysl.add(pom.remove(0).remove(0).name());
+            cnf.addAll(pom);
+
+        }
+
+        for (int i = 0; i < vysl.size(); i++) {
+            Clause kl = new Clause(Literal.Not(a),Literal.Lit(vysl.get(i)));
+            cnf.add(kl);
+
+        }
+
+        for (int i = 0; i < vysl.size(); i++) {
+            c2.add(Literal.Not(vysl.get(i)));
+
+        }
+        cnf.add(c2);
+
+        return cnf;
     }
 }
 
@@ -125,15 +187,44 @@ class Disjunction extends CompositeFormula {
     public Disjunction(Formula[] formulas) {
         super(formulas, "|");
     }
+
+    @Override
+    public Cnf toCnf() {
+        Cnf cnf = new Cnf();
+        String a = Variable.newName();
+        Literal l = Literal.Lit(a);
+        Clause c = new Clause(l);
+        Clause c2 = new Clause(Literal.Not(a));
+        cnf.add(c);
+        ArrayList<String> vysl = new ArrayList<>();
+        for (int i = 0; i < subs.length; i++) {
+            Cnf pom = subs[i].toCnf();
+            vysl.add(pom.remove(0).remove(0).name());
+            cnf.addAll(pom);
+        }
+        for (int i = 0; i < vysl.size(); i++) {
+            Clause kl = new Clause(Literal.Lit(vysl.get(i)),Literal.Not(vysl.get(i)));
+            cnf.add(kl);
+        }
+        for (int i = 0; i < vysl.size(); i++) {
+            Clause kk = new Clause(Literal.Lit(a),Literal.Not(vysl.get(i)));
+            cnf.add(kk);
+            c2.add(Literal.Lit(vysl.get(i)));
+        }
+        cnf.add(c2);
+        return cnf;
+    }
 }
 
 class BinaryFormula extends CompositeFormula {
     BinaryFormula(Formula leftSide, Formula rightSide, String conn) {
         super(new Formula[]{leftSide, rightSide}, conn);
     }
+
     public Formula leftSide() {
         return subf()[0];
     }
+
     public Formula rightSide() {
         return subf()[1];
     }
@@ -143,10 +234,58 @@ class Implication extends BinaryFormula {
     public Implication(Formula a, Formula b) {
         super(a, b, "->");
     }
+
+    @Override
+    public Cnf toCnf() {
+        Cnf cnf = new Cnf();
+        String a = Variable.newName();
+        Literal l = Literal.Lit(a);
+        Clause c = new Clause(l);
+        cnf.add(c);
+        Cnf left = leftSide().toCnf();
+        String le = left.remove(0).remove(0).name();
+        cnf.addAll(left);
+        Cnf rajt = rightSide().toCnf();
+        String r = rajt.remove(0).remove(0).name();
+        cnf.addAll(rajt);
+
+        Clause prvy = new Clause(Literal.Not(a),Literal.Not(le),Literal.Lit(r));
+        Clause druhy = new Clause(Literal.Lit(le),Literal.Lit(a));
+        Clause treti = new Clause(Literal.Not(r),Literal.Lit(a));
+        cnf.add(prvy);cnf.add(druhy);cnf.add(treti);
+
+
+        return cnf;
+    }
 }
 
 class Equivalence extends BinaryFormula {
     public Equivalence(Formula a, Formula b) {
         super(a, b, "<->");
+    }
+
+    @Override
+    public Cnf toCnf() {
+        Cnf cnf = new Cnf();
+        String a = Variable.newName();
+        Literal l = Literal.Lit(a);
+        Clause c = new Clause(l);
+        cnf.add(c);
+        Cnf left = leftSide().toCnf();
+        String le = left.remove(0).remove(0).name();
+        cnf.addAll(left);
+        Cnf rajt = rightSide().toCnf();
+        String r = rajt.remove(0).remove(0).name();
+        cnf.addAll(rajt);
+
+        Clause prvy = new Clause(Literal.Not(a), Literal.Not(le), Literal.Lit(r));
+        Clause druhy = new Clause(Literal.Not(a), Literal.Lit(le), Literal.Not(r));
+        Clause treti = new Clause(Literal.Lit(a), Literal.Not(le), Literal.Not(r));
+        Clause stvrty = new Clause(Literal.Lit(a), Literal.Lit(le), Literal.Lit(r));
+        cnf.add(prvy); cnf.add(druhy);cnf.add(treti);cnf.add(stvrty);
+
+
+
+        return cnf;
     }
 }
